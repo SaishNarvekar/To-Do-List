f3e2fc7e75e2ee57001ff8227eef9bc2f6a75425
Saish Narvekar <narvekar.saish@gmail.com>
2019-02-28 10:39:51 +0530

    Created Project Package

---
+++ todolist/routes.py
@@ -0,0 +1,145 @@
+from todolist import app
+from flask import session, request, render_template, redirect,url_for
+import datetime,time,uuid,hashlib
+from todolist.dbconnection import con
+from todolist.query import get_DataBase_Results, insert_DataBase_Results
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = datetime.timedelta(minutes=15)
+    session.modified = True
+    delete_expired_session()
+
+
+@app.route("/")
+def index():
+    # print(request.remote_addr)
+    # data = get_DataBase_Results("select * from itemlist")
+    if "uuid" in session:
+        update_timestamp()
+        # return render_template("index.html", data=data, session=session)
+        return render_template("index.html",session=session)
+    else:
+        # return render_template("index.html", data=data)
+        return render_template("index.html")
+
+
+@app.route("/private-list")
+def dashboard():
+    return render_template("private-list.html")
+
+# @app.route("/update", methods=['POST'])
+# def update():
+
+#     value = request.form['item']
+#     print(value)
+#     sqlQuery = "Insert Into itemlist(item) VALUES (\"{}\");".format(value)
+#     insert_DataBase_Results(sqlQuery)
+#     return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        hashed_password = hashlib.sha512(
+            request.form['password'].encode('utf-8')).hexdigest()
+        return auth_user(username, hashed_password)
+
+    if "uuid" in session:
+        update_timestamp()
+        expires_timestamp()
+        return redirect(url_for('index'))
+
+    return render_template('login.html', error=False)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if "uuid" in session:
+        update_timestamp()
+        return redirect(url_for('index'))
+
+    if request.method == 'GET':
+        return render_template('register.html', registered=False)
+
+    if request.method == 'POST':
+        email = request.form['email']
+        userName = request.form['username']
+        hashPassWord = hashlib.sha512(
+            request.form['password'].encode('utf-8')).hexdigest()
+        sqlQuery = "Insert Into users(email,username,hashed_password) VALUES (\"{}\",\"{}\",\"{}\");".format(
+            email, userName, hashPassWord)
+        insert_DataBase_Results(sqlQuery)
+        return render_template('register.html', registered=True)
+
+
+@app.route('/logout')
+def logout():
+    insert_DataBase_Results(
+        "delete from session_tracker where uuid = \"{}\"".format(session["uuid"]))
+    session.pop('uuid', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/error/<code>')
+def error(code):
+    return render_template('error.html', code=code)
+
+
+@app.errorhandler(404)
+def notFound(error):
+    return redirect('/error/404')
+
+def auth_user(username, hashed_password):
+    
+    sqlConn = con()
+    cur = sqlConn.cursor(buffered=True)
+    sqlQuery = "SELECT hashed_password from users where username = \"{}\";".format(
+        username)
+    cur.execute(sqlQuery)
+    
+
+    if cur.rowcount == 0:
+        print("Wrong Info")
+        return render_template('login.html', error=True)
+
+    for i in cur.fetchone():
+        if i == hashed_password:
+            print("Valid")
+            userID = uuid.uuid4()
+            session['uuid'] = userID
+            insert_DataBase_Results(
+                "insert into session_tracker (uuid,username) values (\"{}\" ,\"{}\")".format(userID, username))
+            expires_timestamp()
+            return redirect(url_for('index'))
+        else:
+            print("Wrong Password")
+            return render_template('login.html', error=True)
+
+
+def update_timestamp():
+    currentTimestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    sqlQuery = "Update session_tracker set updated_at =\"{}\"".format(
+        currentTimestamp)
+    insert_DataBase_Results(sqlQuery)
+    expires_timestamp()
+
+
+def expires_timestamp():
+    expireTimestamp = (datetime.datetime.now(
+    ) + datetime.timedelta(minutes=15)).strftime('%Y-%m-%d %H:%M:%S')
+    sqlQuery = "Update session_tracker set expires_at =\"{}\"".format(
+        expireTimestamp)
+    insert_DataBase_Results(sqlQuery)
+    # print(expireTimestamp)
+
+
+def delete_expired_session():
+    currentTimestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    sqlQuery = "delete from session_tracker where expires_at <= \"{}\"".format(
+        currentTimestamp)
+    insert_DataBase_Results(sqlQuery)
---
+++ todolist/static/js/login.js
@@ -0,0 +1,21 @@
+var username = document.getElementById('username');
+var password = document.getElementById('password')
+var form  = document.getElementsByTagName('form')[0];
+var username_error = document.getElementById('username-error');
+
+
+username.addEventListener('input',function(event){
+    username_error.style.display = "block";
+    username_error.style.width = "300px";
+    username_error.style.margin = "auto";
+    console.log("Hello World");
+},false);
+
+password.addEventListener('input',function(event){
+    console.log("Hello World");
+},false);
+
+// form.addEventListener("submit", function (event) {
+//     console.log("yes")
+//     event.preventDefault();
+//   }, false);
